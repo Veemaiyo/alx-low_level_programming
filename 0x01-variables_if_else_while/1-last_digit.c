a6315b83e877f05bcc821f30371173bf2fe62785
vee maiyo <Maiyonarukin@gmail.com>
2022-03-11 02:50:37 -0800

    first commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,8 @@
+#!/bin/bash
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-last_digit.c -o 1-last_digit
+./1-last_digit
+Last digit of 629438752 is 2 and is less than 6 and not 0
+./1-last_digit
+Last digit of -748255693 is -3 and is less than 6 and not 0
+ ./1-last_digit
+Last digit of -1052791662 is -2 and is less than 6 and not 0
