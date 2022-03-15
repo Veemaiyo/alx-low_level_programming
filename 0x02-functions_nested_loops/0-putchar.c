c7040dcfc69f4cda0eede69c19f3b0552dc11524
vee maiyo <Maiyonarukin@gmail.com>
2022-03-15 04:22:02 -0700

    nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,6 @@
- gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 0-putchar.c -o 0-putchar
-~/0x02$ ./0-putchar 
+#include "main.h"
+~/0x02$  gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 0-putchar.c -o 0
+-putchar
+~/0x02$ ./0-putchar
 _putchar
-~/0x02$ 
+~/0x02$
