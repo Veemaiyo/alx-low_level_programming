bc1024c90a212b9d86aa37760ab82b97e2dec60f
vee maiyo <Maiyonarukin@gmail.com>
2022-03-17 03:39:57 -0700

    7-prints

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n);
+{
+    print_diagonal(0);
+    print_diagonal(2);
+    print_diagonal(10);
+    print_diagonal(-4);
+    return (0);
+}
