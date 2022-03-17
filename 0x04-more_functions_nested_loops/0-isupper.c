1ccd7028bdc07210f97af75e4eaa2db6c258a8c6
vee maiyo <Maiyonarukin@gmail.com>
2022-03-17 02:54:27 -0700

    1-digit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -15,4 +15,7 @@ int main(void)
     c = 'a';
     printf("%c: %d\n", c, _isupper(c));
     return (0);
-}
+} 
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-isdigit.c -o 1-isdigit./1-isdigit
+0: 1
+a: 0
