bdc014e18228f363499caebb689e501768966adc
vee maiyo <Maiyonarukin@gmail.com>
2022-03-17 05:33:20 -0700

    8-prints

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,14 +1,32 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_square - name of the function
+ * @size: value of ingrese
+ * Return: none
  */
-int main(void)
+
+void print_square(int size)
 {
-    print_square(2);
-    print_square(10);
-    print_square(0);
-    return (0);
+	int a = 0;
+	int b = 0;
+
+	if (size > 0)
+	{
+		while (a < size)
+		{
+			while (b < size)
+			{
+				_putchar('#');
+				b++;
+			}
+			a++;
+			b = 0;
+			_putchar('\n');
+		}
+	}
+	else
+	_putchar('\n');
 }
+
