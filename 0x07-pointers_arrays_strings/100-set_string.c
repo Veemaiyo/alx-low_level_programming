e5b53264b0f824369df35ec1cff9ccc878d13845
vee maiyo <Maiyonarukin@gmail.com>
2022-03-28 06:36:14 -0700

    100 tasks

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - name of the function
+ * @s: pointer to pointer
+ * @to: pointer
+ * Return: none
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
