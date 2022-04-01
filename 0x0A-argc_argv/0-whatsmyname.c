48bc36a661c35a42e46748916ffbee6abde26e8c
vee maiyo <Maiyonarukin@gmail.com>
2022-04-01 04:20:33 -0700

    0 task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - name of the function
+ * @argc: number of command line arguments
+ * @argv: pointer to array content program command lines
+ * Return: none
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
